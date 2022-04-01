199456e29c439a3b469111c86ebab0b0c5aeabdc
sanha <kgpaper@naver.com>
2022-04-02 01:40:01 +0900

    feat: 레이아웃 작성 및 todo 추가 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import moment from 'moment';
-import React from 'react';
+import React, { useState } from 'react';
+import moment, { Moment } from 'moment';
 import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
+import { HiPlusSm } from 'react-icons/hi';
+
+interface TodoListInterface {
+  id: string;
+  isChecked: boolean;
+  content: string;
+}
 
 function App(): JSX.Element {
-  const today = moment();
+  const [todoList, setTodoList] = useState<TodoListInterface[]>([]);
+  const [todoInput, setTodoInput] = useState<string>('');
+
+  const today: Moment = moment();
   const day: string = DAY[today.day() - 1];
-  const adjustedDate = today.format('YYYY년 MM월 DD일');
+  const adjustedDate: string = today.format('YYYY년 MM월 DD일');
+
+  const addTodoElement = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const newElement: TodoListInterface = {
+      id: uuidv4(),
+      isChecked: false,
+      content: todoInput,
+    };
+    setTodoList([...todoList, newElement]);
+  };
 
   return (
     <TodoMain>
@@ -14,6 +35,21 @@ function App(): JSX.Element {
           <TodayInfo>{adjustedDate}</TodayInfo>
           <DayInfo>{day}요일</DayInfo>
         </DateInfoContainer>
+        <TodoElementWrapper>gd</TodoElementWrapper>
+        <TodoInputForm>
+          <TodoInput
+            onChange={(event) => setTodoInput(event.target.value)}
+            value={todoInput}
+            placeholder="할 일을 입력해주세요"
+          />
+          <TodoAddButton
+            onClick={(event) => {
+              addTodoElement(event);
+            }}
+          >
+            <PlusIcon />
+          </TodoAddButton>
+        </TodoInputForm>
       </TodoContainer>
     </TodoMain>
   );
@@ -34,12 +70,15 @@ const TodoMain = styled.main`
 
 const TodoContainer = styled.section`
   width: 70%;
-  padding: 50px 50px 30px;
+  padding: 60px 60px 30px;
   border-radius: 20px;
   background-color: white;
 `;
 
-const DateInfoContainer = styled.div``;
+const DateInfoContainer = styled.div`
+  padding-bottom: 30px;
+  border-bottom: 1px solid #92a9bd;
+`;
 
 const TodayInfo = styled.h2`
   padding-bottom: 20px;
@@ -50,4 +89,38 @@ const TodayInfo = styled.h2`
 const DayInfo = styled.h3`
   color: #92a9bd;
   font-size: 20px;
+`;
+
+const TodoElementWrapper = styled.div`
+  padding: 30px 0;
+  width: 100%;
+  height: 60vh;
+  border-bottom: 1px solid #92a9bd;
+`;
+
+const TodoInputForm = styled.form`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  width: 100%;
+`;
+
+const TodoInput = styled.input`
+  width: 100%;
+  font-size: 18px;
+`;
+
+const TodoAddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  background-color: #92a9bd;
+  border-radius: 30px;
+`;
+
+const PlusIcon = styled(HiPlusSm)`
+  font-size: 32px;
+  color: white;
 `;
---
+++ src/components/TodoElements.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoElements(): JSX.Element {
+  return <div>TodoComponents</div>;
+}
+
+export default TodoElements;
